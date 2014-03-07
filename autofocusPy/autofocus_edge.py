95ef077f225a7a006eb9fc7e923c27020fcc14e9
Yen-Chia Hsu <legenddolphin@gmail.com>
2014-03-07 14:25:34 -0500

    Added a different focusing script.

--- autofocusPy/autofocus_edge.py
+++ autofocusPy/autofocus_edge.py
@@ -1,4 +1,4 @@
-## Compute the sum of differences between every two pixels along the x axis
+## Canny Edge Detection 
 ## This script is used for autofocusing breathecam
 ## Usage: python autofocus.py [IP] [lenseNumber] [isLoop]
 ## e.g. python autofocus.py 192.168.4.13 1 True
@@ -11,7 +11,7 @@ from urllib import urlretrieve
 import socket
 import time
 
-sumDiff_max = 0
+pixels_edge_max = 0
 
 loan_camera_lense_order = ["4","2","1","3"]
 camera2_lense_order = ["1","4","2","3"]
@@ -21,7 +21,7 @@ camera4_lense_order = ["1","3","4","2"]
 ## Change this one according to the camera being used
 current_camera = camera2_lense_order
 
-def computeImage(ip, lenseNumber):
+def getImages(ip, lenseNumber):
     username = "admin"
     password = "illah123"
     addr = username + ":" + password + "@" + ip 
@@ -30,32 +30,34 @@ def computeImage(ip, lenseNumber):
     folderPath = 'images/breathecam/'
     fileName = lenseNumber + '.jpg'
     urlretrieve('http://' + addr + '/image' + current_camera[int(lenseNumber)-1] + '?' + config, folderPath + fileName)
-    computeSumDiff(folderPath, fileName)
+    computeEdge(folderPath, fileName)
     
-def computeSumDiff(folderPath, fileName):
-    global sumDiff_max
+def computeEdge(folderPath, fileName):
+    global pixels_edge_max
     path = folderPath + fileName
     img = cv2.imread(path, 0)
-    kernel = np.array([1, -1])
-    diff = cv2.filter2D(img,-1,kernel)
-    sumDiff = np.sum(diff);
-    if sumDiff_max < sumDiff:
-        sumDiff_max = sumDiff
-    #Image.fromarray(diff).save(folderPath + 'diff_' + fileName)
-    print sumDiff, '/', sumDiff_max, ':', folderPath + fileName
+    edge = cv2.Canny(img, 100, 200)
+    pixels_edge = np.sum(edge) / 255
+    if pixels_edge_max < pixels_edge:
+        pixels_edge_max = pixels_edge
+    Image.fromarray(edge).save(folderPath + 'edge_' + fileName)
+    print 'Edges :', pixels_edge, '/', pixels_edge_max, ':', folderPath + fileName
+    return [img, edge, pixels_edge]
 
-def computeAllImages(folderPath):
-    computeSumDiff(folderPath, '1.jpg')
-    computeSumDiff(folderPath, '2.jpg')
-    computeSumDiff(folderPath, '3.jpg')
-    computeSumDiff(folderPath, '4.jpg')
+def computeAllEdges(folderPath):
+    [img1, edge1, pixels_edge1] = computeEdge(folderPath, '1.jpg')
+    [img2, edge2, pixels_edge2] = computeEdge(folderPath, '2.jpg')
+    [img3, edge3, pixels_edge3] = computeEdge(folderPath, '3.jpg')
+    [img4, edge4, pixels_edge4] = computeEdge(folderPath, '4.jpg')
+    pixels_edge = pixels_edge1 + pixels_edge2 + pixels_edge3 + pixels_edge4
+    return pixels_edge
 
 if __name__ == '__main__':
     if len(sys.argv) > 3:
         if sys.argv[3] == 'True':
             while True:
-                computeImage(sys.argv[1], sys.argv[2])
+                getImages(sys.argv[1], sys.argv[2])
         else:
-            computeImage(sys.argv[1], sys.argv[2])
+            getImages(sys.argv[1], sys.argv[2])
     else:
-        computeAllImages('images/test/')
+        computeAllEdges('images/test/')
