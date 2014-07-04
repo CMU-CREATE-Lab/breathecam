a4b72c506a5d9778a35cd61841502b0ff9aa4266
konduri <kondurivamshi@gmail.com>
2014-07-03 22:56:41 -0400

    Optimizer giving promising results. First commit after getting a good result

--- latLonToPixelXY/LatLongToPixel.py
+++ latLonToPixelXY/LatLongToPixel.py
@@ -121,33 +121,27 @@ pixel_from_ecef(lla2ecef(home_plate_lla), heinz_imodel)
 
 
 
-
-
-
-
-#listOfKnownPoints =  []
-#listOfKnownPoints.append(({'lat':40.447057,'lon': -80.006170,'alt': 222  },{'xPixel': 1244.64  , 'yPixel': 2002.26 }))
 #TODO if necessary:write parsing script to read from json file into datastructures
 listOfKnownPoints = [
-({'lat':40.441366,'lon': -79.994775, 'alt': 222  },{'xPixel': 4144.083  , 'yPixel': 760.187  }),
-({'lat':40.441823,'lon': -80.012776, 'alt': 206  },{'xPixel': 2752.402  , 'yPixel': 1642.679 }),
-({'lat':40.444209,'lon': -80.009189, 'alt': 243  },{'xPixel': 2645.539  , 'yPixel': 1307.134 }),
-({'lat':40.438913,'lon': -80.011329, 'alt': 222  },{'xPixel': 4265.726  , 'yPixel': 1557.211 }),
-({'lat':40.445889,'lon': -80.015228, 'alt': 212  },{'xPixel': 1133.514  , 'yPixel': 1447.93  }),
-({'lat':40.436114,'lon': -80.018811, 'alt': 231  },{'xPixel': 8372.991  , 'yPixel': 1288.094 }),
-({'lat':40.437190,'lon': -80.017578, 'alt': 213  },{'xPixel': 6084.036  , 'yPixel': 1382.086 }),
-({'lat':40.437054,'lon': -80.016465, 'alt': 234  },{'xPixel': 5754.627  , 'yPixel': 1463.708 }),
-({'lat':40.432715,'lon': -79.989217, 'alt': 231  },{'xPixel': 5258.228  , 'yPixel': 1074.379 }),
-({'lat':40.441829,'lon': -80.003713, 'alt': 234  },{'xPixel': 3736.978  , 'yPixel': 1274.988 })]
+({'lat':40.446247,'lon': -80.004498, 'alt': 221  },{'xPixel': 3920  , 'yPixel': 1528 }),
+({'lat':40.443270,'lon': -80.004716, 'alt': 299  },{'xPixel': 1772  , 'yPixel': 1308 }),
+({'lat':40.447973,'lon': -80.002204, 'alt': 255  },{'xPixel': 5156  , 'yPixel': 1185 }),
+({'lat':40.447769,'lon': -80.000556, 'alt': 257  },{'xPixel': 5865  , 'yPixel': 1188 }),
+({'lat':40.445897,'lon': -79.998206, 'alt': 248  },{'xPixel': 7366  , 'yPixel': 1371 }),
+({'lat':40.446126,'lon': -80.007509, 'alt': 256  },{'xPixel': 2893  , 'yPixel': 1228 }),
+({'lat':40.449067,'lon': -79.997707, 'alt': 239  },{'xPixel': 6735  , 'yPixel': 1134 }),
+({'lat':40.444333,'lon': -80.000954, 'alt': 290  },{'xPixel': 5914  , 'yPixel': 1502 }),
+({'lat':40.453250,'lon': -80.003403, 'alt': 266  },{'xPixel': 5084  , 'yPixel': 939  }),
+({'lat':40.451433,'lon': -79.998886, 'alt': 276  },{'xPixel': 6155  , 'yPixel': 941  })]
 
 #paramters to the cost function that need to be optimized. pixels per radian, and quarternion
 #We start of with the following guess
-parameters = [2855 , 1 , 0 , 0 , 0]
+parameters = [2855 , 0.1414 , 0.1414 , 0.1414 , 0.1414]
 
-#Functions takes in the paramerters and spits out the total cost(error). All the points that we handselected are put in the global varibale "listOfKnownPoints" which is used in the function.
+
 def totalErrorInPixels(para):
     error = 0
-    global errorList        #I use it to print out the error list and the end of optimization.
+#    global errorList        #I use it to print out the error list and the end of optimization.
     errorList = []
     cam_model = {
         'height': 1733,            
@@ -163,34 +157,29 @@ def totalErrorInPixels(para):
 	pixelCalc  = pixel_from_ecef(lla2ecef(llaKnown),cam_model)
         errorList.append((pixelKnown[0] - pixelCalc[0]  ,  pixelKnown[1] - pixelCalc[1]))
   	error      = error  + (pixelKnown[0] - pixelCalc[0])**2 + (pixelKnown[1] - pixelCalc[1])**2
-#    print "The errors obtained with these parameters are ", errorList , " \n   "
+  #  print "The errors obtained with these parameters are ", errorList , " \n   "
     return error
 
 
 
-#Function written to get leastsq in optimize to work. Can be safely ignored
-latLong  = {'lat':40.441366,'lon': -79.994775, 'alt': 222  }
-pixelVal = (4144.083, 760.187 )
+latLongTest1  = {'lat':40.447309,'lon': -80.003277, 'alt': 245  }
+pixelValTest1 = (4635, 1280 )
+latLongTest2  = {'lat':40.447510,'lon': -80.009500, 'alt': 259  }
+pixelValTest2 = (2951, 1111 )
 
-def errorOnePoint(para,latLong,pixelVal):
-#latLong is a dictionary of lat,long and alt. example = {'lat':40.447057, 'lon':-80.006170, 'alt':222}
-#pixelVal is a tuple  (xpixel,ypixel)
+#For testing
+def errorForOnePoint(para,latLong,pixelVal):
     cam_model = {
-        'height': 1733,            
+        'height': 1733,   #duplicating the data from previous function. Will try to avoid it once code is good for all other purposes         
         'width' : 8970 ,
         'pixels_per_radian': para[0], 
         'ecef': lla2ecef(heinz_camera_lla),
         'rotation': (para[1], para[2], para[3], para[4])
     }
     pixelCalc     = pixel_from_ecef(lla2ecef(latLong),cam_model)
-    errorOnePoint = (pixelVal[0] - pixelCalc[0])**2 + (pixelVal[1] - pixelCalc[1])**2
+    errorOnePoint = math.fabs(pixelVal[0] - pixelCalc[0]) +math.fabs(pixelVal[1] - pixelCalc[1])
+    print "The pixel value observed is", pixelCalc 
     return errorOnePoint
-
-
-#Test to check if the function runs without error
-#testCostFunction =  totalErrorInPixels(parameters)
-#print "The cost of the points observed is  ", testCostFunction
-
 
 #Various Optimizers attempted.
 
@@ -199,17 +188,14 @@ def errorOnePoint(para,latLong,pixelVal):
 res = scipy.optimize.fmin(totalErrorInPixels, parameters, maxiter = 10**6,maxfun = 10**6 ,ftol = 0.00001, xtol = 0.00001)
 # check http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin.html downhill simplex algorithm
 
-global errorList
-print "The optimum parameters found are " , res , " and the error observed is  ", errorList
-IPython.embed()
+#global errorList
+#print "The optimum parameters found are " , res , " and the error observed is  ", errorList
 
 #To test the data
-normQuat   =  normalize(res[1:])
-newRes     = [res[0]] +  list(normQuat)
-print "The new Res looks like " , newRes
-print "\n The total error is "  , totalErrorInPixels(newRes) , " \n and the list of error values is ", errorList
-print "\n error of one location is " , errorOnePoint(newRes,latLong,pixelVal)
-
+normQuat                =  normalize(res[1:])
+parametersOptimized     =  [res[0]] +  list(normQuat)
+print "Error of test location one " , errorOnePoint(parametersOptimized , latLongTest1 , pixelValTest1)
+print "Error of test location two " , errorOnePoint(parametersOptimized , latLongTest2 , pixelValTest2)
 
 
 
