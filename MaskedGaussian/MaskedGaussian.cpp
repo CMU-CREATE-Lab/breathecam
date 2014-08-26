1546fecac8a2a0300622f52abc5cfe80f80b888d
konduri <kondurivamshi@gmail.com>
2014-08-26 19:32:23 -0400

    using Inpaints now.Much simpler

--- MaskedGaussian/MaskedGaussian.cpp
+++ MaskedGaussian/MaskedGaussian.cpp
@@ -7,23 +7,31 @@ int main(int argc, char** argv )
     Mat     image,mask,blurred,clone,output;
     image = imread(argv[1],1);
     mask  = imread(argv[2],0);
+    Mat inpainted;
+    inpaint(image, mask, inpainted, 30, CV_INPAINT_TELEA);
+    imwrite("inpaintedImage.jpg",inpainted);
+/*
 
-    if ( !image.data | !mask.data)
-    {
-        std::cout << ("No image data in one of arguments\n");
-        return -1;
-    }
-    else
-    {
         output = image.clone();
         mask  = Scalar::all(255) - mask;
+        imshow("See",mask);
+
+        Mat inpainted;
+        std::cout << "Trying to inpaint \n"; 
+        inpaint(image, mask, inpainted, 3, CV_INPAINT_TELEA);
+        imshow("inpainted image", inpainted);
+        std::cout << "We inpainted \n";  
+
+        imwrite("inpaintedImage.jpg",inpainted);
+        waitKey(0);
+
         output.copyTo(mask, mask);
         blur(mask, blurred,Size(75,75));
         output.copyTo(blurred,mask);
         GaussianBlur(blurred, output, Size(5,5),2.5);
         image.copyTo(output,mask);
         imwrite( "masked_blurred.jpg", output );
-        waitKey(0);
+//        waitKey(0);
+*/
         return 0;
-    }
 }
