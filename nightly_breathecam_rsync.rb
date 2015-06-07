8ae7b26abe83e073a663f7aa4cc6223602ea0946
Paul Dille <pdille@cmucreatelab.org>
2015-06-07 19:42:32 -0400

    - With new storing scheme, we need to ensure that parent directories are created before rsyncing

--- nightly_breathecam_rsync.rb
+++ nightly_breathecam_rsync.rb
@@ -9,12 +9,14 @@ previous_day = (Date.today - 1).to_s
 
 src_tm_path  = "#{src_root}/#{previous_day}.timemachine"
 year_month_day = previous_day.split("-")
-final_output_tm_path = "#{dest_path}/#{year_month_day[0]}/#{year_month_day[1]}/#{previous_day}.timemachine"
+parent_output_path = "#{dest_path}/#{year_month_day[0]}/#{year_month_day[1]}"
+final_output_tm_path = "#{parent_output_path}/#{previous_day}.timemachine"
 tmp_output_tm_path = final_output_tm_path + ".tmp"
 
 system("find #{src_tm_path} -type f -name '*.mp4' -exec qtfaststart {} \\;")
 puts "Rsyncing #{src_tm_path}/ to #{host}:/#{tmp_output_tm_path}"
-is_success = system("rsync -a #{src_tm_path}/ #{host}:/#{tmp_output_tm_path}")
+
+is_success = system("rsync -a --rsync-path='mkdir -p #{parent_output_path} && rsync' #{src_tm_path}/ #{host}:/#{tmp_output_tm_path}")
 if !is_success
   puts "Error rsyncing."
   exit
