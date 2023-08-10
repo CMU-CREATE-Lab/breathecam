b074dd161ba7dc30979e8fda579a01170f891fa3
Paul Dille <pdille@cmucreatelab.org>
2023-08-10 19:12:44 -0400

    - Allow for querying of multiple camera ids under camera_findings

--- breathecam.cmucreatelab.org/www/app/controllers/camera_findings_controller.rb
+++ breathecam.cmucreatelab.org/www/app/controllers/camera_findings_controller.rb
@@ -7,13 +7,13 @@ class CameraFindingsController < ApplicationController
   def index
     begin_time_epoch = params[:begin_time]
     end_time_epoch = params[:end_time]
-    camera_id = params[:camera_id]
+    camera_ids = (params[:camera_id] || "").gsub(/\s+/, "").split(",")
 
     begin_time = begin_time_epoch ? Time.at(begin_time_epoch.to_i) : Time.at(0)
     end_time = end_time_epoch ? Time.at(end_time_epoch.to_i) : Time.now
 
     @camera_findings = CameraFinding.where("begin_time >= ? AND end_time <= ?", begin_time, end_time)
-    @camera_findings = @camera_findings.where("camera_id = ?", camera_id) if camera_id
+    @camera_findings = @camera_findings.where(camera_id: camera_ids) unless camera_ids.blank?
     @camera_findings = @camera_findings.order("begin_time")
 
     render :json => @camera_findings, :layout => false
